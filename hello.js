c54e745eedae99af405e9a3d03d1d021c41bd3a8
Rytham <rythamcwork@gmail.com>
2024-10-28 20:09:00 +0530

    add revert console log

--- hello.js
+++ hello.js
@@ -2,9 +2,6 @@ console.log("Hello ! Git") ;
 console.log("Hello ! Gituuu") ;
 
 console.log("Hello about to try git reset huh") ; 
-console.log("Hello from dev-rytham , making git reset work") ; 
-
-console.log("The code breaks here") ; 
 
 
-console.log("trying out revert") ; 
+console.log("Trying out REVERTTT") ; 
